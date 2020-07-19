e5099cb4c02617312fe9ad350e02eb621ee42ccb
therexone <renerjake@gmail.com>
2020-07-19 20:09:13 +0530

    feat: setup devextreme react grid basic demo, removed more cra boilerplate code, added cors to backend, minor refactors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './styles/_globals.css'
 
 ReactDOM.render(
   <React.StrictMode>
