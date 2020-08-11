bb05f9dded871d655a03956c5fabcdb355bb7cd4
randomlylelo <lzhang.0693@gmail.com>
2020-08-11 14:10:58 -0400

    refactor create, modify, and delete team into own file.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,13 @@
 /* eslint-disable no-shadow */
-import React, { useEffect, useState } from "react";
-import { Typography, Tabs, Tab, Button, Container } from "@material-ui/core";
+import React, { useState } from "react";
+import { Typography, Tabs, Tab, Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-  Grid,
-  Table,
-  TableHeaderRow,
-} from "@devexpress/dx-react-grid-material-ui";
 import { TabPanel, a11yProps } from "./components/Tabs";
+import CreateTeam from "./Tabs/CreateTeam";
+import DeleteTeam from "./Tabs/DeleteTeam";
+import ModifyTeam from "./Tabs/ModifyTeam";
 
-const url = "http://localhost:8080/test";
-
-/**
- *
- * @param {String} name
- */
-const makeTitleReadable = (name) => {
-  return name === "_id"
-    ? "Id"
-    : name
-        .split("_")
-        .map((word) => {
-          // console.log(word);
-          if (word) return word[0].toUpperCase() + word.substring(1);
-          return "";
-        })
-        .join(" ");
-};
+// const url = "http://localhost:8080/test";
 
 const useStyles = makeStyles(() => ({
   background: {
@@ -36,32 +17,11 @@ const useStyles = makeStyles(() => ({
 
 const App = () => {
   const classes = useStyles();
-  const [data, setData] = useState([]);
-  const [columns, setColumns] = useState([]);
   const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-  useEffect(() => {
-    const fetchData = async (url) => {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data;
-    };
-    fetchData(url)
-      .then((data) => {
-        setData(data);
-        const cols = Object.keys(data[0]).map((item) => ({
-          name: item,
-          title: makeTitleReadable(item),
-        }));
-        setColumns(cols);
-      })
-      // eslint-disable-next-line no-console
-      .catch((e) => console.log(e));
-  }, []);
 
   return (
     <Container maxWidth="lg" className={classes.background}>
@@ -80,32 +40,13 @@ const App = () => {
         <Tab label="Modify Team" {...a11yProps(2)} />
       </Tabs>
       <TabPanel value={value} index={0}>
-        {/* Put real create team code here. */}
-        <Button color="primary" variant="contained">
-          Add to Group
-        </Button>
-        <Grid rows={data} columns={columns}>
-          <Table />
-          <TableHeaderRow />
-        </Grid>
+        <CreateTeam />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <Button color="primary" variant="contained">
-          Add to Group
-        </Button>
-        <Grid rows={data} columns={columns}>
-          <Table />
-          <TableHeaderRow />
-        </Grid>
+        <DeleteTeam />
       </TabPanel>
       <TabPanel value={value} index={2}>
-        <Button color="primary" variant="contained">
-          Add to Group
-        </Button>
-        <Grid rows={data} columns={columns}>
-          <Table />
-          <TableHeaderRow />
-        </Grid>
+        <ModifyTeam />
       </TabPanel>
     </Container>
   );
