859c67e10d4aa299ca532daf73bd4770fc0139c4
toduko <teddyepich@gmail.com>
2020-08-11 17:58:13 +0300

    Made column titles look readable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import {
-  Grid,
-  Table,
-  TableHeaderRow,
+	Grid,
+	Table,
+	TableHeaderRow,
 } from '@devexpress/dx-react-grid-material-ui';
 
-const url = 'http://localhost:8080/test'
+const url = 'http://localhost:8080/test';
+
+/**
+ *
+ * @param {String} name
+ */
+const makeTitleReadable = (name) => {
+	return name === '_id'
+		? 'Id'
+		: name
+				.split('_')
+				.map((word) => {
+					console.log(word);
+					if (word) return word[0].toUpperCase() + word.substring(1);
+					else return '';
+				})
+				.join(' ');
+};
 
 function App() {
-  const [data, setData] = useState([])
-  const [columns, setColumns] = useState([])
+	const [data, setData] = useState([]);
+	const [columns, setColumns] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async (url) => {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data;
-    }
-    fetchData(url).then((data) => {
-      setData(data);
-      let cols = Object.keys(data[0]).map(
-        (item) => ({ name: item, title: item })
-      );
-      setColumns(cols)
-      console.log(cols)
-    }).catch((e) => console.log(e))
-  }, [])
+	useEffect(() => {
+		const fetchData = async (url) => {
+			const response = await fetch(url);
+			const data = await response.json();
+			return data;
+		};
+		fetchData(url)
+			.then((data) => {
+				setData(data);
+				let cols = Object.keys(data[0]).map((item) => ({
+					name: item,
+					title: makeTitleReadable(item),
+				}));
+				setColumns(cols);
+			})
+			.catch((e) => console.log(e));
+	}, []);
 
-  return (
-    <div className="App">
-      TACHYON FRONTEND
-      <Paper>
-        <Grid
-          rows={data}
-          columns={columns}
-        >
-          <Table />
-          <TableHeaderRow />
-        </Grid>
-      </Paper>
-    </div>
-  );
+	return (
+		<div className='App'>
+			TACHYON FRONTEND
+			<Paper>
+				<Grid rows={data} columns={columns}>
+					<Table />
+					<TableHeaderRow />
+				</Grid>
+			</Paper>
+		</div>
+	);
 }
 
 export default App;
